9ac60f2e159ca5130cb3b00e8b4f51cf12a9da59
Macropodus <orangeandy12@gmail.com>
2018-11-23 23:09:42 +0800

    Assignment_I_I

--- Assignment_I.py
+++ Assignment_I.py
@@ -1,8 +1,33 @@
-# pattern
+# pattern assignment
 import numpy as np
 import sklearn
 import cv2
 
 img = cv2.imread('duck_data.jpg')  # open file
-cv2.imshow('image', img)
-cv2.waitKey(0)
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # RBG to gray
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+
+img_pixel = np.array(img)
+# print(img_pixel)
+
+pixel = img.shape
+height = pixel[0]
+width = pixel[1]
+# print(x, y)
+
+# remove the background
+for i in range(height):
+    for j in range(width):
+        if img_pixel[i][j] < 230:
+            img_pixel[i][j] = 0
+        else:
+            continue
+
+img = img_pixel.reshape(img_pixel.shape[0], img_pixel.shape[1])
+cv2.imwrite('BackgroundRemove.jpg', img)
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
