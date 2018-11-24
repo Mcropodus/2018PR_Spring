c11952174c64a629cf316090271740e9cb861d40
Macropodus <orangeandy12@gmail.com>
2018-11-24 19:15:39 +0800

    Assignment_I_II

--- Assignment_I.py
+++ Assignment_I.py
@@ -20,14 +20,31 @@ width = pixel[1]
 # print(x, y)
 
 # remove the background
+array = []  # save the data
 for i in range(height):
     for j in range(width):
         if img_pixel[i][j] < 230:
             img_pixel[i][j] = 0
+            array.append(0)
         else:
+            array.append(1)
             continue
 
 img = img_pixel.reshape(img_pixel.shape[0], img_pixel.shape[1])
 cv2.imwrite('BackgroundRemove.jpg', img)
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
+cv2.imshow('image', img)
+cv2.waitKey(0)
+array = np.reshape(array, (height, width))
+print(array)
+
+
+# array = []
+# for i in range(height):
+#     for j in range(width):
+#         if img_pixel[i][j] == 0
+#             array.append(0)
+#         else:
+#             array.append(1)
+#
+# print(array)
+
