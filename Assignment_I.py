05c4fb3c223363daac58af34b2b3488cbf192aee
Macropodus <orangeandy12@gmail.com>
2018-11-25 15:05:46 +0800

    Assignment_I_IV

--- Assignment_I.py
+++ Assignment_I.py
@@ -3,22 +3,29 @@ import numpy as np
 from sklearn import naive_bayes
 from sklearn import model_selection as ms
 import cv2
+import matplotlib.pyplot as plt
 
 img = cv2.imread('duck_data.jpg')  # open file
+non_duck_img = cv2.imread('non_duck.jpg')  # the data without duck
 # cv2.imshow('image', img)
 # cv2.waitKey(0)
 
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # RBG to gray
-# cv2.imshow('image', img)
+non_duck_img = cv2.cvtColor(non_duck_img, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('image', non_duck_img)
 # cv2.waitKey(0)
 
 img_pixel = np.array(img)
+n_img_pixel = np.array(non_duck_img)
 # print(img_pixel.shape)
 # print(img_pixel)
 
 pixel = img.shape
 height = pixel[0]
 width = pixel[1]
+non_duck_pixel = non_duck_img.shape
+n_height = non_duck_pixel[0]
+n_width = non_duck_pixel[1]
 # print(height, width)
 # print(x, y)
 
@@ -33,15 +40,46 @@ for i in range(height):
             array.append(1)
             continue
 
+n_array = []  # remove the background without duck
+for i in range(n_height):
+    for j in range(n_width):
+        if n_img_pixel[i][j] < 230:
+            n_img_pixel[i][j] = 0
+            n_array.append(0)
+        else:
+            n_array.append(0)
+            continue
+
 img = img_pixel.reshape(img_pixel.shape[0], img_pixel.shape[1])  # array to image
 cv2.imwrite('BackgroundRemove.jpg', img)
-# cv2.imshow('image', img)
+n_img = n_img_pixel.reshape(n_img_pixel.shape[0], n_img_pixel.shape[1])
+# cv2.imshow('image', n_img)
 # cv2.waitKey(0)
 img_pixel = np.reshape(img_pixel, (height*width, 1))  # reshape the train data
 # array = np.reshape(array, (height*width, 1))
 # print(array)
 
+# for non_duck data
+n_img_pixel = np.reshape(n_img_pixel, (n_height*n_width, 1))
 
+
+x_train, x_test, y_train, y_test = ms.train_test_split(
+    img_pixel, array, test_size=0.1
+)
 model_naive = naive_bayes.GaussianNB()  # Gaussian model
-model_naive.fit(img_pixel, array)
-print('scoring the naive Bayes : ', model_naive.score(img_pixel, array))
+model_naive.fit(x_train, y_train)
+model_naive.fit(n_img_pixel, n_array)
+print('scoring the naive Bayes : ', model_naive.score(x_test, y_test))
+
+test = cv2.imread('full_duck.jpg')
+test = np.array(test)
+test = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
+Pixel = test.shape
+t_height = Pixel[0]
+t_width = Pixel[1]
+test = np.reshape(test, (t_height*t_width, 1))
+y_proba = model_naive.predict(test)
+# print(y_proba)
+result = y_proba.reshape(t_height, t_width)
+cv2.imshow('result', result)
+cv2.waitKey(0)
