edc1f11800575df6f8a33765dfa55122609de3eb
Macropodus <orangeandy12@gmail.com>
2018-11-25 16:02:56 +0800

    Assignment_I_V

--- Assignment_I.py
+++ Assignment_I.py
@@ -33,7 +33,7 @@ n_width = non_duck_pixel[1]
 array = []  # save the label data
 for i in range(height):
     for j in range(width):
-        if img_pixel[i][j] < 230:
+        if img_pixel[i][j] < 225:
             img_pixel[i][j] = 0
             array.append(0)
         else:
@@ -43,7 +43,7 @@ for i in range(height):
 n_array = []  # remove the background without duck
 for i in range(n_height):
     for j in range(n_width):
-        if n_img_pixel[i][j] < 230:
+        if n_img_pixel[i][j] < 225:
             n_img_pixel[i][j] = 0
             n_array.append(0)
         else:
@@ -62,13 +62,19 @@ img_pixel = np.reshape(img_pixel, (height*width, 1))  # reshape the train data
 # for non_duck data
 n_img_pixel = np.reshape(n_img_pixel, (n_height*n_width, 1))
 
+ori_img = cv2.imread('duck_data.jpg')
+ori_img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2GRAY)
+ori_img = np.reshape(ori_img, (height*width, 1))
+ori_n_img = cv2.imread('non_duck.jpg')
+ori_n_img = cv2.cvtColor(ori_n_img, cv2.COLOR_BGR2GRAY)
+ori_n_img = np.reshape(ori_n_img, (n_height*n_width, 1))
 
 x_train, x_test, y_train, y_test = ms.train_test_split(
-    img_pixel, array, test_size=0.1
+    ori_img, array, test_size=0.1
 )
 model_naive = naive_bayes.GaussianNB()  # Gaussian model
 model_naive.fit(x_train, y_train)
-model_naive.fit(n_img_pixel, n_array)
+model_naive.partial_fit(ori_n_img, n_array)
 print('scoring the naive Bayes : ', model_naive.score(x_test, y_test))
 
 test = cv2.imread('full_duck.jpg')
@@ -78,8 +84,18 @@ Pixel = test.shape
 t_height = Pixel[0]
 t_width = Pixel[1]
 test = np.reshape(test, (t_height*t_width, 1))
-y_proba = model_naive.predict(test)
-# print(y_proba)
-result = y_proba.reshape(t_height, t_width)
-cv2.imshow('result', result)
-cv2.waitKey(0)
+y_pred = model_naive.predict(test)
+# for i in range(t_height*t_width):
+#     if y_pred[i] == 1:
+#         print(1)
+result = np.reshape(y_pred, (t_height, t_width))
+# print(result)
+for i in range(t_height):
+    for j in range(t_width):
+        if result[i][j] > 0:
+            result[i][j] = 255
+        else:
+            continue
+cv2.imwrite('result.jpg', result)
+# cv2.imshow('result', result)
+# cv2.waitKey(0)
